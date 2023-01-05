7f1a539b205ba32860e91e3a918cc48bac8675c4
Nithya-code <121249703+Nithya-code@users.noreply.github.com>
2023-01-05 14:18:46 +0530

    Add files via upload

---
+++ website1.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+        $('.navbar ul').toggle();
+    });
+});
+
+
+function myfunction(){
+    alert ("congratulations! your Feedback submit successfully");
+ }
+
